fc4d94aceb9a6b743dfa5b0b64ddd4597d050d3c
antelocode <antelocode@gmail.com>
2024-10-20 12:42:23 +0700

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,48 +1,4 @@
 export const dataWork=[
-    // {
-    //     id:1,
-    //     name:"Lofipharse",
-    //     exercise: true,
-    //     projectNote: "",
-    //     date: "3/9/23",
-    //     image:"lofipharse.jpg",
-    //     github: null,
-    //     live:"https://lofi-pharse.vercel.app/",
-    //     tech: ["react js","TailwindCSS"]
-    // },
-    // {
-    //     id:2,
-    //     name:"TourEase",
-    //     exercise: true,
-    //     projectNote: "",
-    //     date: "3/10/23",
-    //     image:"tourease.jpg",
-    //     github: null,
-    //     live:"https://tour-ease-5j6rmf45f-syegi.vercel.app/",
-    //     tech: ["react js", "TailwindCSS"]
-    // },
-    {
-        id:3,
-        name:"CineHunt",
-        exercise: true,
-        projectNote: "",
-        date: "11/10/23",
-        image:"cinehunt.jpg",
-        github:null,
-        live:"https://cinehunt.vercel.app/",
-        tech: ["react js","TailwindCSS"]
-    },
-    // {
-    //     id:4,
-    //     name:"4clean",
-    //     exercise: false,
-    //     projectNote: "Practical assignments",
-    //     date: "8/9/22",
-    //     image:"4clean.jpg",
-    //     github:"https://github.com/4clean/web",
-    //     live:"https://4clean.github.io/web/",
-    //     tech: ["HTML","CSS","Javascript"]
-    // },
     {
         id:4,
         name:"Tokosl 88",
@@ -52,17 +8,6 @@ export const dataWork=[
         image:"tokosl88.jpg",
         live:"https://www.tokosl88.com/",
         tech: ["Laravel", "TailwindCSS", "HTML"]
-    },
-    {
-        id: 6,
-        name:"CornQR",
-        exercise: true,
-        projectNote: "",
-        date: "12/12/23",
-        github : "https://github.com/AryaTjiu/cornqr",
-        image:"cornqr.jpg",
-        live:"https://cornqr.vercel.app/",
-        tech: ["Next js","TailwindCSS"]
     },
     {
         id: 7,
